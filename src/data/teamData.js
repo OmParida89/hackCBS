773ab6d3aecd1309040589a4e3274b3e7025d5e7
Om Parida <om.25741@sscbs.du.ac.in>
2026-09-01 00:15:45 +0530

    MTC

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -2,67 +2,67 @@ export const teamMembers = [
   {
     name: "Ronak Choudhary",
     role: "Lead Organizer",
-    img: "/assets/img/crew/team/organizer-1.webp",
+    img: "/assets/img/crew/team/ronak.webp",
     linkedin: "https://www.linkedin.com/in/ronak-choudhary-bb855b327/"
   },
   {
     name: "Gaurav Singh",
     role: "Lead Organizer",
-    img: "/assets/img/crew/team/organizer-2.webp",
+    img: "/assets/img/crew/team/gaurav.webp",
     linkedin: "https://www.linkedin.com/in/gauruvksingh/"
   },
   {
     name: "Samarth Gupta",
     role: "Organizer",
-    img: "/assets/img/crew/team/organizer-3.webp",
+    img: "/assets/img/crew/team/samarth.webp",
     linkedin: "https://www.linkedin.com/in/samarth-gupta-891886353/"
   },
   {
     name: "Saanvi Arora",
     role: "Organizer",
-    img: "/assets/img/crew/team/organizer-4.webp",
+    img: "/assets/img/crew/team/saanvi.webp",
     linkedin: "https://www.linkedin.com/in/saanvi-arora-805b79324/"
   },
   {
     name: "Ajay Kumar",
     role: "Organizer",
-    img: "/assets/img/crew/team/organizer-5.webp",
+    img: "/assets/img/crew/team/ajay.webp",
     linkedin: "https://www.linkedin.com/in/ajay-kumar-48a706326/"
   },
   {
     name: "Abhinav Harsh",
     role: "Organizer",
-    img: "/assets/img/crew/team/organizer-6.webp",
+    img: "/assets/img/crew/team/abhinav.webp",
     linkedin: "https://www.linkedin.com/in/abhinavharshdhn/"
   },
   {
     name: "Nitish Rathore",
     role: "Organizer",
-    img: "/assets/img/crew/team/organizer-7.webp",
+    img: "/assets/img/crew/team/nitish.webp",
     linkedin: "https://www.linkedin.com/in/nitish-rathore-a49477259/"
   },
   {
     name: "Atharv Sharma",
     role: "Organizer",
-    img: "/assets/img/crew/team/organizer-8.webp",
+    img: "/assets/img/crew/team/atharv.webp",
     linkedin: "https://www.linkedin.com/in/atharv-sharma-183587342/"
   },
   {
     name: "Hemant Chaudhari",
     role: "Organizer",
-    img: "/assets/img/crew/team/organizer-9.webp",
+    img: "/assets/img/crew/team/hemant.webp",
     linkedin: "https://www.linkedin.com/in/hemant-chaudhari-002789220/"
   },
   {
     name: "Aditi Sachdeva",
     role: "Organizer",
-    img: "/assets/img/crew/team/organizer-10.webp",
+    img: "/assets/img/crew/team/aditi.webp",
     linkedin: "https://www.linkedin.com/in/aditi-sachdeva-539954328/"
   },
   {
     name: "Vansh Dewan",
     role: "Organizer",
-    img: "/assets/img/crew/team/organizer-11.webp",
+    img: "/assets/img/crew/team/vansh.webp",
     linkedin: "https://www.linkedin.com/in/vansh-dewan-1ba789334/"
   }
 ];
